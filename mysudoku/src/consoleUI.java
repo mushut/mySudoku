725c573afc9ae7a606c0837e8e9be4d33e9f0a6a
Aki Sipovaara <mushut@gmail.com>
2018-08-28 02:45:24 +0300

    Started the project in IDE.

---
+++ mysudoku/src/consoleUI.java
@@ -0,0 +1,10 @@
+/* mySudoku
+ * Aki Sipovaara 2018
+ */
+
+// Console UI for using the application
+public class consoleUI {
+    public static void main(String[] args) {
+        System.out.println("mySudoku");
+    }
+}
---
+++ mysudoku/src/mySudoku.java
@@ -0,0 +1,7 @@
+/* mySudoku
+ * Aki Sipovaara 2018
+ */
+
+public class mySudoku {
+    // Contains sudoku and ways to use it
+}
