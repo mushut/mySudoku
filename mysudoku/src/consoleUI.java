e2ec151c4b08093e2ff1cb1d2112280f418b8e17
Aki Sipovaara <mushut@gmail.com>
2018-08-31 16:23:54 +0300

    Worked on console UI.

--- mysudoku/src/consoleUI.java
+++ mysudoku/src/consoleUI.java
@@ -2,9 +2,52 @@
  * Aki Sipovaara 2018
  */
 
+import java.util.Scanner;
+
 // Console UI for using the application
 public class consoleUI {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int menuChoice = 0;
+        mySudoku sudoku;
+
         System.out.println("mySudoku");
+        while (true) {
+            System.out.println("Menu");
+            System.out.println("1: Create a sudoku");
+            System.out.println("2: Print the sudoku");
+            System.out.println("3: Save the sudoku in a file");
+            System.out.println("4: Load the sudoku from a file");
+            System.out.println("5: Quit");
+            menuChoice = input.nextInt();
+
+            switch(menuChoice) {
+                case 1:
+                    // Create a sudoku
+                    sudoku = new mySudoku();
+                    break;
+                case 2:
+                    // Print the sudoku if there is one created
+                    sudoku.print();
+                    break;
+                case 3:
+                    // Save the sudoku in a file if there is one created
+                    sudoku.saveToFile();
+                    break;
+                case 4:
+                    // Load the sudoku from a file
+                    sudoku = loadSudokuFromFile();
+                    break;
+                case 5:
+                    // Quit
+                    break;
+                default:
+                    // Is this needed?
+            }
+
+            if (menuChoice == 5) {
+                break;
+            }
+        }
     }
 }
